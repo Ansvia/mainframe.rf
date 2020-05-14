8dbf1f7a2a5dcc6243081aef0b172f63079ff7b2
Robin Syihab <obin.mf@gmail.com>
2020-05-14 16:06:45 +0700

    Update deploy-server.sh

--- etc/script/deploy-server.sh
+++ etc/script/deploy-server.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+#
+# Ini adalah script untuk deploy/update server, script ini akan otomatis meng-upload
+# source code ke server di path $$name_shout_snake_case$_DEST_SERVER_PATH (konfigurasinya bisa 
+# di lihat di ./ansible/group_vars/all.yml).
+#
+# Setelah upload selesai, kode Rust akan dicompile di sisi server lalu akan menginstall
+# servis menggunakan Systemd, semua prosedur ini bisa dilihat di ansible playbook file api.yml
+#
+# Kemudia servis akan dijalankan apabila belum jalan dan atau akan direstart apabila 
+# servis sedang berjalan. Sehingga service akan otomatis jalan dengan versi terbaru.
+#
+# Troubleshooting:
+#
+#   1. Pastikan server target telah terinstall Python agar Ansible bisa bekerja di sisi server.
+#   2. Pastikan Rust compiler telah terinstall di server target.
+#   3. Pastikan Diesel CLI telah terinstall di server target agar migration script bisa dijalankan otomatis.
+#
+#
 
 export CURDIR=`dirname $0`
 . $CURDIR/includes.sh
